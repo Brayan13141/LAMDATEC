a1b7478470508479372d65de0862139dcc947cae
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-10-17 11:14:22 -0600

    SE MODIFICO PARA QUE FUNCIONE CON LA PLANTILLA

--- app/src/main/java/com/example/lamdatec/Interfaz/Pantallas/Pantalla_Principal.kt
+++ app/src/main/java/com/example/lamdatec/Interfaz/Pantallas/Pantalla_Principal.kt
@@ -20,32 +20,8 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun PantallaInicio(navController: NavHostController) {
-    val scaffoldState = rememberScaffoldState()
-    val coroutineScope = rememberCoroutineScope()
+    PPantallas(navController,"BIENVENIDO A LAMDATEC")
+    {
 
-    Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = {
-            TopAppBar(
-                title = { Text("BIENVENIDO A LAMDATEC") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        coroutineScope.launch {
-                            scaffoldState.drawerState.open()  // Abre el menú desplegable
-                        }
-                    }) {
-                        Icon(Icons.Default.Menu, contentDescription = "Abrir menú")
-                    }
-                }
-            )
-        },
-        drawerContent = {
-            DrawerMenu(navController = navController, scaffoldState = scaffoldState)
-        },
-        content = { paddingValues ->
-            Column(modifier = Modifier.padding(paddingValues)) {
-                Text("Bienvenido a la pantalla principal", modifier = Modifier.padding(16.dp))
-            }
-        }
-    )
+    }
 }
