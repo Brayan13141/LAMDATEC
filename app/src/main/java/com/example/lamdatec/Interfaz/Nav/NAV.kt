ccdba3c4c448c5d5318839c5771c2ab5e58834ba
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-10-17 11:16:39 -0600

    Nada

--- app/src/main/java/com/example/lamdatec/Interfaz/Nav/NAV.kt
+++ app/src/main/java/com/example/lamdatec/Interfaz/Nav/NAV.kt
@@ -1,63 +1,22 @@
 package com.example.lamdatec.Interfaz.Nav
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.ListItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.lamdatec.Interfaz.Pantallas.PantallaConGrafico
 import com.example.lamdatec.Interfaz.Pantallas.PantallaInicio
-import com.example.lamdatec.R
-import com.google.android.libraries.intelligence.acceleration.Analytics
-import kotlinx.coroutines.launch
 
 @Composable
 fun App(
 ) {
-    val navController = rememberNavController()
+        val navController = rememberNavController()
             NavHost(
                 navController = navController,   // El NavController que estás usando
                 startDestination = PantallasNav.PRINCIPAL.route   // La pantalla que será tu punto de inicio
             ) {
                 composable( PantallasNav.PRINCIPAL.route) { PantallaInicio(navController) }
-                composable( PantallasNav.S1.route) { PantallaInicio(navController) }
+                composable( PantallasNav.S1.route) {  PantallaConGrafico(navController) }
 
             }
 }
