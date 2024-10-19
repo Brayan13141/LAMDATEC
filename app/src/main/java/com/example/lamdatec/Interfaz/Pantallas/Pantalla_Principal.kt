d9a6cc727497feafa18e096dadca0eb81d79f7dd
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-10-18 23:44:35 -0600

    IMPORTS

--- app/src/main/java/com/example/lamdatec/Interfaz/Pantallas/Pantalla_Principal.kt
+++ app/src/main/java/com/example/lamdatec/Interfaz/Pantallas/Pantalla_Principal.kt
@@ -1,21 +1,8 @@
 package com.example.lamdatec.Interfaz.Pantallas
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import kotlinx.coroutines.launch
+import com.example.lamdatec.Interfaz.Pantallas.Plantilla.PPantallas
 
 
 @Composable
--- app/src/main/java/com/example/lamdatec/Interfaz/Pantallas/Plantilla/Plantilla_Pantallas.kt
+++ app/src/main/java/com/example/lamdatec/Interfaz/Pantallas/Plantilla/Plantilla_Pantallas.kt
@@ -1,9 +1,6 @@
-package com.example.lamdatec.Interfaz.Pantallas
+package com.example.lamdatec.Interfaz.Pantallas.Plantilla
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,7 +14,6 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
