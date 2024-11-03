00ceb00411c39d8f727d5b9c26d900d43e0351cb
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-02 19:38:19 -0600

    REPOSITORIO DE LA Pantalla_Principal.kt

---
+++ app/src/main/java/com/example/lamdatec/features/pPrincipal/data/Ppantalla_Repo_Fire.kt
@@ -0,0 +1,47 @@
+package com.example.lamdatec.features.pPrincipal.data
+
+
+import co.yml.charts.common.model.Point
+import com.google.firebase.database.*
+import kotlinx.coroutines.tasks.await
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class Ppantalla_Repo_Fire {
+
+    private val database = FirebaseDatabase.getInstance().getReference("LAMDATEC")
+
+    suspend fun setFechaHoraActual() {
+        val currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        val currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
+        database.child("/Sensores/VAR_CONTROL/FECHA_ACTUAL/").setValue(currentDate).await()
+        database.child("/Sensores/VAR_CONTROL/HORA_ACTUAL/").setValue(currentTime).await()
+    }
+
+    fun obtenerValorSensor(ruta: String, callback: (Float) -> Unit) {
+
+        database.child(ruta).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val valor = snapshot.child("Valor").getValue(Float::class.java) ?: 0f
+                callback(valor)
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    fun obtenerEstadoMoto(callback: (Boolean) -> Unit) {
+        database.child("Sensores/Estatus").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val estado = snapshot.getValue(Boolean::class.java) ?: false
+                callback(estado)
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    suspend fun cambiarEstadoMoto(estado: Boolean) {
+        database.child("Sensores/Estatus").setValue(estado).await()
+    }
+}
