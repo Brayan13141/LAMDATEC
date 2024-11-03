f3b7f845adc4080ad70dbc8256720c7a944ccd7c
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-02 22:23:10 -0600

    SE MODIFICO EL CODIGO EN BASE A LAS NECESIDADES DE CADA COMPOSABLE(NECESIDADES:VIEWMODEL)

--- app/src/main/java/com/example/lamdatec/core/navigation/NAV.kt
+++ app/src/main/java/com/example/lamdatec/core/navigation/NAV.kt
@@ -1,33 +1,49 @@
 package com.example.lamdatec.core.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.lamdatec.Interfaz.Pantallas.PantallaConGrafico
+import com.example.lamdatec.Interfaz.Pantallas.GraficoMQ2_VISTA
+import com.example.lamdatec.features.Graficos.MQ2.viewM_MQ2
+import com.example.lamdatec.features.Graficos.MQ2.viewM_MQ7
+import com.example.lamdatec.features.Graficos.MQ7.GraficoMQ7_VISTA
 import com.example.lamdatec.features.pPrincipal.presentation.Pantalla.PantallaInicio
 import com.example.lamdatec.features.authentication.presentation.login.LoginScreen
+import com.example.lamdatec.features.components.PantallaConGraficoGENERAL
 
 @Composable
 fun App(
 ) {
     val navController = rememberNavController()
-            NavHost(
-                navController = navController,   // El NavController que estás usando
-                startDestination = PantallasNav.LOGIN.route   // La pantalla que será tu punto de inicio
-            ) {
-                composable( PantallasNav.PRINCIPAL.route) { PantallaInicio(navController) }
-                composable( PantallasNav.S1.route) {  PantallaConGrafico(navController) }
-                composable( PantallasNav.S2.route) {  PantallaConGrafico(navController) }
-                composable( PantallasNav.LOGIN.route) {
-                    LoginScreen() { email, password ->
-                    // Aquí puedes agregar tu lógica de autenticación (ejemplo: Firebase)
-                    // Si el inicio de sesión es exitoso, navega a la pantalla principal
-                    navController.navigate(PantallasNav.PRINCIPAL.route) {
-                        popUpTo(PantallasNav.LOGIN.route) { inclusive = true } // Evita volver a la pantalla de login
-                    }
-                }
+    val viewModelMQ7: viewM_MQ7 = viewModel()
+    val viewModelMQ2: viewM_MQ2 = viewModel()
+
+    NavHost(
+        navController = navController,   // El NavController que estás usando
+        startDestination = PantallasNav.LOGIN.route   // La pantalla que será tu punto de inicio
+    ) {
+        composable(PantallasNav.PRINCIPAL.route) { PantallaInicio(navController) }
+        composable(PantallasNav.SENSOR_MQ2.route) {
+            GraficoMQ2_VISTA(navController, viewModelMQ2)
+        }
+        composable(PantallasNav.SENSOR_MQ7.route) {
+            GraficoMQ7_VISTA(navController, viewModelMQ7)
+        }
+        composable(PantallasNav.LOGIN.route) {
+            LoginScreen() { email, password ->
+                // Navergar a la pantalla principal(FALTAN VALIDACIONES)
+                navController.navigate(PantallasNav.PRINCIPAL.route) {
+
+                    popUpTo(PantallasNav.LOGIN.route) {
+                        inclusive = true
+                    } // Evita volver a la pantalla de login
+
                 }
             }
+        }
+    }
 }
 
