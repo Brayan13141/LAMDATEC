bfbad6ac0ad0247dd553cb56f88e617fd927e173
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-02 19:50:58 -0600

    REPOSITORIO DEL Grafico_2.kt

---
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/data/Grafico_MQ2_REPO.kt
@@ -0,0 +1,35 @@
+package com.example.lamdatec.features.Graficos.MQ2.data
+
+import android.util.Log
+import com.google.firebase.database.*
+import co.yml.charts.common.model.Point
+import kotlinx.coroutines.tasks.await
+
+class SensorMQ2Repository {
+    private val database = FirebaseDatabase.getInstance().getReference("LAMDATEC")
+
+    fun consultarDatosSensores(actualizar: (List<Point>) -> Unit) {
+        var airValues : List<Float> = listOf()
+        val database = FirebaseDatabase.getInstance().reference
+
+        database.child("LAMDATEC/Sensores/SENSORES/MQ2/Valor").get().addOnSuccessListener { snapshot ->
+            database.child("LAMDATEC/Sensores/SENSORES/MQ2/Valor").addValueEventListener(object : ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val valor = snapshot.getValue(Int::class.java) ?: 0
+                    airValues = airValues + valor.toFloat()
+                    actualizar(airValues.mapIndexed { index, value ->
+                        Point(index.toFloat(), value)
+                    })
+                    Log.e("Firebase", "Valor actualizado en Firebase: $valor")
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+        }.addOnFailureListener { exception ->
+            Log.e("Firebase", "Error al leer el valor actual de 'air': ${exception.message}")
+        }
+
+    }
+}
