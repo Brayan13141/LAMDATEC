e87579c19192cffeb9deefdee14bcc121a9527d6
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-02 22:27:38 -0600

    IMPORTS

--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/viewM_MQ2.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/viewM_MQ2.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import co.yml.charts.common.model.Point
 import com.example.lamdatec.features.Graficos.MQ2.data.SensorMQ2Repository
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 class viewM_MQ2(private val repository: SensorMQ2Repository = SensorMQ2Repository()) : ViewModel() {
     var puntosGrafico = MutableStateFlow<List<Point>>(emptyList())
