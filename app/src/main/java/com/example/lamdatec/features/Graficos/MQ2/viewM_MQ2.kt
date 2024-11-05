7f8cc78ec9f3f2118f88c2754608ad215bb212b5
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-05 09:46:09 -0600

    SE AGREGO UN viewModelScope.launch

--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/viewM_MQ2.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/viewM_MQ2.kt
@@ -1,19 +1,23 @@
 package com.example.lamdatec.features.Graficos.MQ2
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import co.yml.charts.common.model.Point
 import com.example.lamdatec.features.Graficos.MQ2.data.SensorMQ2Repository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class viewM_MQ2(private val repository: SensorMQ2Repository = SensorMQ2Repository()) : ViewModel() {
-    var puntosGrafico = MutableStateFlow<List<Point>>(emptyList())
+    var puntosGrafico = MutableStateFlow<List<Point>>(listOf())
         private set
 
     init {
-        consultarDatosSensores()
+        viewModelScope.launch {
+            consultarDatosSensores()
+        }
     }
 
-    private fun consultarDatosSensores() {
+    private suspend fun consultarDatosSensores() {
         repository.consultarDatosSensores { puntos ->
             puntosGrafico.value = puntos // Actualiza el estado de los puntos del gráfico
         }
--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ7/viewM_MQ7.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ7/viewM_MQ7.kt
@@ -1,19 +1,24 @@
 package com.example.lamdatec.features.Graficos.MQ2
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import co.yml.charts.common.model.Point
 import com.example.lamdatec.features.Graficos.MQ7.data.SensorMQ7Repository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
 class viewM_MQ7(private val repository: SensorMQ7Repository = SensorMQ7Repository()) : ViewModel() {
     var puntosGrafico = MutableStateFlow<List<Point>>(emptyList())
         private set
 
 
     init {
-        consultarDatosSensores()
+        viewModelScope.launch {
+            consultarDatosSensores()
+        }
     }
 
-    private fun consultarDatosSensores() {
+    private suspend fun consultarDatosSensores() {
         repository.consultarDatosSensores { puntos ->
             puntosGrafico.value = puntos // Actualiza el estado de los puntos del gráfico
         }
