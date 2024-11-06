a9438f45584ff73032115c390418a2aa4ea037bf
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-06 17:43:49 -0600

    SE MODIFICO LA FUNCION PARA REGRESAR 2 VALORES LA LISTA Y EL VALOR EN REAL

--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/data/Grafico_MQ2_REPO.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/data/Grafico_MQ2_REPO.kt
@@ -7,37 +7,40 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 interface SensorMQ2Repository {
-    fun consultarDatosSensores(actualizar: (List<Point>) -> Unit)
+    fun consultarDatosSensores(actualizar: (Pair<List<Point>, Float>) -> Unit)
 }
 
 @Singleton
 class SensorMQ2RepositoryImp @Inject constructor(
     private val db: FirebaseDatabase
 ) : SensorMQ2Repository {
-
-    override fun consultarDatosSensores(actualizar: (List<Point>) -> Unit) {
+    override fun consultarDatosSensores(actualizar: (Pair<List<Point>, Float>) -> Unit) {
         var airValues: List<Float> = listOf()
 
         // Acceso a la referencia de la base de datos
-        // listener para conexion exitosa
+        // Listener para conexión exitosa
         db.reference.child("LAMDATEC/Sensores/SENSORES/MQ2/Valor").get().addOnSuccessListener {
-            // listener para obtener los valores
+            // Listener para obtener los valores en tiempo real
             db.reference.child("LAMDATEC/Sensores/SENSORES/MQ2/Valor")
                 .addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(snapshot: DataSnapshot) {
                         val valor = snapshot.getValue(Int::class.java) ?: 0
                         airValues = airValues + valor.toFloat()
-                        actualizar(airValues.mapIndexed { index, value ->
+
+                        // Convertimos airValues a una lista de puntos y lo enviamos junto con valor
+                        val puntos = airValues.mapIndexed { index, value ->
                             Point(index.toFloat(), value)
-                        })
+                        }
+                        actualizar(Pair(puntos, valor.toFloat()))
+
                         Log.e("Firebase", "Valor actualizado en Firebase: $valor")
                     }
 
                     override fun onCancelled(error: DatabaseError) {
                         // Manejo del error
+                        Log.e("Firebase", "Error al obtener datos: ${error.message}")
                     }
                 })
-            // listener para fallo en conexion
         }.addOnFailureListener { exception ->
             Log.e("Firebase", "Error al leer el valor actual de 'air': ${exception.message}")
         }
