119eba6270173babc9c641f28df7360c50548b0a
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-06 17:44:47 -0600

    SE IMPLEMENTO EL REPO PARA LA INYECCION DE DEPENDENCIAS

--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ7/data/Grafico_MQ7_REPO.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ7/data/Grafico_MQ7_REPO.kt
@@ -3,12 +3,22 @@ package com.example.lamdatec.features.Graficos.MQ7.data
 import android.util.Log
 import com.google.firebase.database.*
 import co.yml.charts.common.model.Point
+import dagger.hilt.android.scopes.ViewModelScoped
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
 import kotlinx.coroutines.tasks.await
 
-class SensorMQ7Repository {
-   suspend fun consultarDatosSensores(actualizar: (List<Point>) -> Unit) {
+interface SensorMQ7Repository {
+    fun consultarDatosSensores(actualizar: (List<Point>) -> Unit)
+}
+
+@ViewModelScoped
+class SensorMQ7RepositoryImp @Inject constructor(
+    private val db: FirebaseDatabase
+) : SensorMQ7Repository {
+    override fun consultarDatosSensores(actualizar: (List<Point>) -> Unit) {
         var airValues : List<Float> = listOf()
-        val database = FirebaseDatabase.getInstance().reference
+        val database = db.reference
 
         database.child("LAMDATEC/Sensores/SENSORES/MQ7/Valor").get().addOnSuccessListener { snapshot ->
             database.child("LAMDATEC/Sensores/SENSORES/MQ7/Valor").addValueEventListener(object : ValueEventListener {
