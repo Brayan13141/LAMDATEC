de7b0a5fcee54c3277a6d6e4ed9e735b36c0378c
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-05 21:24:37 -0600

    IMPORT

--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/Grafico_MQ2_VISTA.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/Grafico_MQ2_VISTA.kt
@@ -6,7 +6,6 @@ import androidx.navigation.NavHostController
 
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lamdatec.features.Graficos.MQ2.MQ2ViewModel
 import com.example.lamdatec.features.components.PantallaConGraficoGENERAL
 
