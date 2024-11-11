615a8e9e711a919a5c363b372cb8b8223c341be7
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2024-11-11 13:01:32 -0600

    SE AGREGO LA FUNCION PARA OBTENER LAS FECHAS POR FILTRO, POR AHORA ES LA FECHA ACTUAL SOLAMENTE,TAMBIEN LA LISTA DE LOS DIAS Y LOS MESES, LA FUNCION DE LIMPIAR LOS PUNTOS

--- app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/MQ2ViewModel.kt
+++ app/src/main/java/com/example/lamdatec/features/Graficos/MQ2/MQ2ViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.lamdatec.features.Graficos.MQ2
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import co.yml.charts.common.model.Point
+import com.example.lamdatec.features.Graficos.MQ2.data.FiltrosFecha
 import com.example.lamdatec.features.Graficos.MQ2.data.SensorMQ2RepositoryImp
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,18 +17,61 @@ class MQ2ViewModel @Inject constructor(
 
     var puntosGrafico = MutableStateFlow<List<Point>>(emptyList())
         private set
-
     var valorActual = MutableStateFlow<Float>(0f)
         private set
+    //------------------------------------------------------------------------------------------
+    var valorActualFiltro = MutableStateFlow<FiltrosFecha>(FiltrosFecha.NINGUNO)
+        private set
 
-    init {
-        consultarDatosSensores()
+    var Dias = MutableStateFlow<List<String>>(emptyList())
+        private set
+    var Meses = MutableStateFlow<List<String>>(emptyList())
+        private set
+    //------------------------------------------------------------------------------------------
+    fun limpiarPuntos() {
+        puntosGrafico.value = emptyList()
     }
 
-    private fun consultarDatosSensores() {
-        repository.consultarDatosSensores { (puntos, valor) ->
-            puntosGrafico.value = puntos       // Actualiza el estado de los puntos del gráfico
-            valorActual.value = valor          // Actualiza el estado del valor actual
+    init {
+        if (valorActualFiltro.value == FiltrosFecha.NINGUNO) {
+            consultarDatosSensores()
+            obtenerFechasDisponibles()
+        }
+    }
+
+    fun consultarDatosSensores() {
+        viewModelScope.launch {
+            repository.consultarDatosSensores { (puntos, valor) ->
+                if (valorActualFiltro.value == FiltrosFecha.NINGUNO) {
+                    puntosGrafico.value =
+                        puntos       // Actualiza el estado de los puntos del gráfico
+                    valorActual.value = valor          // Actualiza el estado del valor actual
+                }
+            }
+        }
+    }
+
+    fun consultarDatosConFiltro(filtro: FiltrosFecha) {
+        viewModelScope.launch {
+            valorActualFiltro.value = filtro
+            repository.consultarDatosSensoresConFiltro(filtro, dia = 0) { (puntos, valor) ->
+                if (valorActualFiltro.value != FiltrosFecha.NINGUNO) {
+                    puntosGrafico.value =
+                        puntos       // Actualiza el estado de los puntos del gráfico
+                    valorActual.value = valor          // Actualiza el estado del valor actual
+                }
+
+            }
+        }
+
+    }
+
+    fun obtenerFechasDisponibles() {
+        viewModelScope.launch {
+            repository.obtenerFechasDisponibles { (fechasR, valorM) ->
+                    Dias.value = fechasR
+                    Meses.value = valorM
+            }
         }
     }
 }
